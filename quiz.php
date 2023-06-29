fdc7bedf5a1606d203b724d5c76ff0e02b6b2553
JRaulSierra <j.raulsierra@gmail.com>
2023-06-28 18:20:55 -0600

    feat: create init page for teacher create quiz

---
+++ quiz.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pantalla de Inicio</title>
+  
+  <!-- Agregar los enlaces a los archivos CSS de Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Creación de Cuestionario</a>
+  </nav>
+
+  <div class="container mt-4">
+    <h1>Bienvenido, ¿desea crear un cuestionario?</h1>
+
+    <button class="btn btn-primary" onclick="location.href='createQuiz.php'">Crear Cuestionario</button>
+  </div>
+
+  <!-- Agregar los enlaces a los archivos JavaScript de Bootstrap (jQuery primero, luego Popper.js y luego Bootstrap) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
